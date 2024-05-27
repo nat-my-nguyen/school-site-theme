dba5cf2013b48b91ef73e62c5e3490943ff9a1f8
cchan395 <156140027+cchan395@users.noreply.github.com>
2024-05-27 00:56:26 -0700

    added two taxonomy files for designers and developers. created archive-school-student file with code to render the categories

--- archive-school-student.php
+++ archive-school-student.php
@@ -20,23 +20,72 @@ get_header();
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
+				//to render designers category
+				$args = array(
+					'post_type' => 'school-student',
+					'posts_per_page' => -1,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'school-student-category',
+							'field' => 'slug',
+							'terms' => 'designers'
+						)
+					)
+				);
+				//while loop will run through all Students posts
+				$query = new WP_Query($args);
+				if($query->have_posts()){
+					echo '<section class="design-section"><h2>'.esc_html__('Designers') . '</h2>';
+					while($query -> have_posts()){
+						$query -> the_post();
+						?>
+						<article>
+							<a href="<?php the_permalink(); ?>">
+								<h2><?php the_title();?></h2>
+								<?php the_post_thumbnail('large'); ?>
+							</a>
+							<?php the_excerpt(); ?>
+						</article>
+						<?php
+					}
+					wp_reset_postdata();
+					echo '</section>';
+				}
+				//to render developers category
+				$args = array(
+					'post_type' => 'school-student',
+					'posts_per_page' => -1,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'school-student-category',
+							'field' => 'slug',
+							'terms' => 'developers'
+						)
+					)
+				);
+				//while loop will run through all Student posts
+				$query = new WP_Query($args);
+				if($query->have_posts()){
+					echo '<section class="dev-section"><h2>Developers</h2>';
+					while($query -> have_posts()){
+						$query -> the_post();
+						?>
+						<article>
+							<a href="<?php the_permalink(); ?>">
+								<h2><?php the_title();?></h2>
+								<?php the_post_thumbnail('large'); ?>
+							</a>
+							<?php the_excerpt(); ?>
+						</article>
+						<?php
+					}
+					wp_reset_postdata();
+					echo '</section>';
+				}
+			
+			?>
+		<?php
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
