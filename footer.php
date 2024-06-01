f4a0c9ed89eab33b89389973d94dfa3459badbad
Natalia N. <156234685+nat-my-nguyen@users.noreply.github.com>
2024-05-31 19:42:36 -0700

    Stylings for Footer: - fixed the gap from the default stylings _posts-and-pages that affected the footer - moved any mixins to _mixins for organization

--- footer.php
+++ footer.php
@@ -16,19 +16,24 @@
 			<?php the_custom_logo(); ?>
 		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'school-site' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'school-site' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+			<h3>Credits</h3>
+			<p>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'school-site' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'school-site' ), 'WordPress' );
+					?>
+				</a>
+			</p>
+			<p>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'school-site' ), 'school-site', '<a href="https://carolc.sgedu.site/school-site">Natalia Nguyen and Carol Chan</a>' );
 				?>
+			</p>
 		</div><!-- .site-info -->
 		<div class="footer-menu">
+			<h3>Links</h3>
 			<nav>
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-right',) ); ?>
 			</nav>
